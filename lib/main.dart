4c8d4af572d24f6f122758c19875238b3d25bf10
Reiko-Developer <lukassfifa@gmail.com>
2020-07-06 07:42:11 -0500

    Starting the 4th module.
    Starting the creation of the Expenses planner.

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/quiz.dart';
 
-import './result.dart';
-import './quiz.dart';
+void main() => runApp(_MyHomeApp());
 
-void main() => runApp(MyApp());
-
-// TODO: make it easier.
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 6},
-        {'text': 'Green', 'score': 4},
-        {'text': 'White', 'score': 1}
-      ],
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Snake', 'score': 10},
-        {'text': 'Lion', 'score': 7},
-        {'text': 'Elephant', 'score': 4},
-        {'text': 'Rabbit', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'Who\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 10},
-        {'text': 'Limeira', 'score': 7},
-        {'text': 'Macilon', 'score': 7},
-        {'text': 'Claudionor', 'score': 3}
-      ],
-    },
-  ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    } else {
-      print('No more questions!');
-      print('Total score: ' + _totalScore.toString());
-    }
-  }
-
+class _MyHomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text("Título"),
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, _resetQuiz),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Chart'),
+              ),
+            ),
+            Card(
+              child: Text("List of TX"),
+              color: Colors.redAccent,
+            )
+          ],
+        ),
       ),
     );
   }
