3bdba410c503d849dd6d0827ba26c4b983250e60
Reiko-Developer <lukassfifa@gmail.com>
2020-07-07 08:15:06 -0500

    Theming the app
    Using Theme styles on the entire app and using custom fonts too.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+      ),
       home: MyHomePage(),
     );
   }
